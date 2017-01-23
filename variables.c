1acba7d28dd971176786c433d3ffee9509d4693a
Hector Flores <hecto932@gmail.com>
2017-01-23 13:13:20 -0600

    Hello world and variables

---
+++ variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+	// My code
+	int x;
+	int y;
+	float pi;
+	int suma;
+
+	x = 3;
+	y = 5;
+
+	suma = x + y;
+
+	printf("x(%d) + y(%d) = %d\n", x,y, suma);
+
+	return 0;
+}
