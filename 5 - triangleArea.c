e0386f069a2fe64b6decc08e68cc955c7f2591ca
Hector Flores <hecto932@gmail.com>
2017-01-23 16:59:02 -0600

    Triangle area

---
+++ 5 - triangleArea.c
@@ -0,0 +1,15 @@
+// Triangle area
+// area = base * altura / 2
+// %g remove the zeros
+
+#include <stdio.h>
+
+int main() {
+	float base, altura, area;
+
+	base = 5.25;
+	altura = 17.32;
+	area = (base * altura) / 2;
+
+	printf("Area: %g \n", area);
+}
