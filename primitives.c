ffcd0cc443287339c15b2443dd9d6ed69480156e
Hector Flores <hecto932@gmail.com>
2017-01-23 16:26:03 -0600

    Primitive types

---
+++ primitives.c
@@ -0,0 +1,31 @@
+// TYPES
+
+// NUMEROS ENTEROS:
+// - char(-128, +127) 1 byte
+// - short(-32768, 32,767) 2 bytes 
+// - int(-32768, 32,767) 2 bytes 
+// - long
+
+// NUMEROS REALES(enteros o con coma)
+// - float 
+// - double
+
+#include <stdio.h>
+
+int main(){
+
+	int entero1, entero2;
+	entero1 = 13;
+	entero2 = 15000;
+	float g = 9.8, pi = 3.145;
+	char mi_inicial = 'H';
+	char otra_inicial = 'F';
+
+	printf("entero1: %d\n", entero1);
+	printf("g: %f\n", g);
+	printf("Inicial: %c\n", mi_inicial);
+	
+
+
+	return 0;
+}
