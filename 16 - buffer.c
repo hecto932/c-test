d09053ae4c69b7eb7f1e2de926e07d07b147c48e
Hector Flores <hecto932@gmail.com>
2017-01-23 22:59:31 -0600

    Buffer

---
+++ 16 - buffer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+	int a;
+	int b;
+
+	fflush(stdin);
+
+	printf("First value?\n");
+	scanf("%d", &a);
+
+	fflush(stdin)
+
+	printf("First value?\n");
+	scanf("%d", &a);
+
+	printf("\n a = %d", a);
+	printf("\n b = %d", b);
+
+
+	return 0;
+}
